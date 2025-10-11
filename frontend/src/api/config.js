8258406fa33e5dbf9745bba2c76e90ea100974e0
sayan14banerjee <banerjeesayan554@gmail.com>
2025-10-11 09:59:18 +0530

    update

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,6 +1,6 @@
 // config.js
 const CONFIG = {
-  API_URL: import.meta.env.VITE_API_URL || "https://expense-tracker-nunu.onrender.com/"
+  API_URL: import.meta.env.VITE_API_URL || "https://expense-tracker-nunu.onrender.com"
 };
 
 export default CONFIG;
