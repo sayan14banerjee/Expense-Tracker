89ffed533c2a2273ba1b6f40981d928078cea5ac
sayan14banerjee <banerjeesayan554@gmail.com>
2025-10-11 08:52:43 +0530

    update for deploy

--- backend/app/database.py
+++ backend/app/database.py
@@ -9,5 +9,5 @@ if not MONGO_URI:
     raise ValueError("No Mongo URI found in environment variables!")
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
-db = client.get_default_database() if "/" in MONGO_URI else client.expense_tracker
+db = client["expense_tracker"]
 print(f"✅ Connected to MongoDB at {MONGO_URI}")
--- backend/app/utils/auth.py
+++ backend/app/utils/auth.py
@@ -16,10 +16,12 @@ ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/users/login")
 
 def hash_password(password: str) -> str:
+    # bcrypt only supports up to 72 bytes
+    password = password[:72]
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    return pwd_context.verify(plain_password, hashed_password)
+    return pwd_context.verify(plain_password[:72], hashed_password)
 
 def create_access_token(data: dict, expires_delta: timedelta = None): # type: ignore
     to_encode = data.copy()
